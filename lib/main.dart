134205c74ccd16e9506e13ed77ad998a94a916a7
steelwsky <zero.rise@yandex.ru>
2019-10-31 19:33:53 +1000

    branch for adding provider 1. %, +-, AC->C are done in a 'dirty' way. 2. Find out how to set auto sizing fontSize in textView. 3. Different icon's sizes 4. I want to try put here a provider (state management). Reading'n'Exploring.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 
 void main() => runApp(MyApp());
-
+//adad
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
